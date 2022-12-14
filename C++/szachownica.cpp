cc30b26faac8cd13548d7486e53fc41852139e66
markpal <marekp1980@gmail.com>
2022-12-14 19:46:09 +0100

    Create szachownica.cpp

---
+++ C++/szachownica.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+#define N 8
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    
+    for(i=0; i<N; i++){
+      for(j=0; j<N; j++)
+        cout << (((i+j)%2 == 0) ? 'X' : ' ');  // te nawiasy sa wazne, sprobuj bez
+      cout << endl;    
+    }
+    
+    return 0;
+}
