0aafd505cf8fa03191f700be6021893a32cf2025
markpal <marekp1980@gmail.com>
2022-12-14 19:16:26 +0100

    Update animacja.cpp

--- C++/animacja.cpp
+++ C++/animacja.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::chrono; 
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+
+
+int main()
+{
+    int i = 0;
+
+    while(1)
+    {
+        if(i % 2 == 0)
+        {
+            cout << "   O   \n";
+            cout << "  /|\\  \n";
+            cout << "  / \\  \n";
+        }
+        else
+        {
+            cout << "  \\O/   \n";
+            cout << "   |  \n";
+            cout << "  / \\  \n";  
+        }
+        sleep_for(0.5s);
+        cout << "\033[2J";
+        i++;
+    }
+    
+
+    return 0;
+}
 
